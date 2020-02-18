16fd3ef659203ebd727a1c32fb571205353ef7e6
Georgi Kolev <kolev9605@gmail.com>
2020-02-18 17:20:05 +0200

    Revert test commit.

--- FitnessTracker.Api/Startup.cs
+++ FitnessTracker.Api/Startup.cs
@@ -34,7 +34,6 @@ namespace FitnessTracker.Api
             Configuration = configuration;
         }
 
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
