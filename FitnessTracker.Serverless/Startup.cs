b353795d760759798169dd1a52b7d788800cb090
Georgi Kolev <kolev9605@gmail.com>
2020-06-29 15:02:04 +0300

    Revert Cognito

--- FitnessTracker.Serverless/Startup.cs
+++ FitnessTracker.Serverless/Startup.cs
@@ -29,25 +29,6 @@ namespace FitnessTracker.Serverless
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthorization(
-                options =>
-                {
-                    options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme).Build();
-                    options.AddPolicy("Admins", policy => policy.Requirements.Add(new CognitoGroupAuthorizationRequirement("")));
-                });
-
-            services.AddSingleton<IAuthorizationHandler, CognitoGroupAuthorizationHandler>();
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.Audience = "9ihse57eq084705o1b51abd6t";
-                options.Authority = "https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_D5AxOojHX";
-                options.RequireHttpsMetadata = false;
-            });
-
             services.AddControllers();
 
             // Add S3 to the ASP.NET Core dependency injection framework.
